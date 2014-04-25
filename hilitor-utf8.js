98198442ff0253a2e92eb0ea61c696906b4a3345
Ger Hobbelt <ger@hobbelt.com>
2014-04-25 18:51:49 +0200

    and include the UTF8 one as per http://www.the-art-of-web.com/javascript/search-highlight/ comment by Yanosh Kunsh:
    ---
    
    > Yanosh Kunsh 19 September, 2013
    
    Thank you for this great tool.
    
    Something that I found useful was to add a utf-8 string comparison capability. This was needed because I wanted to search for strings in languages other than English.
    
    It contains the following changes:
    
    1. Two additional functions were added: `convertCharStr2jEsc` and `dec2hex4`: these are used to convert all non-ASCII characters to escaped UTF8 charactes. For example the string 'понеделник' will be converted to '\u043F\u043E\u043D\u0435\u0434\u0435\u043B\u043D\u0438\u043A'. The original source of these functions can be found [here](http://rishida.net/tools/conversion/).
    
    2. In the 'apply' method the following line is added at the top of the function:
    
            input = convertCharStr2jEsc(input);
    
    3. In the `setRegex` method the line that used to read:
    
            input = input.replace(/^[^\w]+|[^\w]+$/g, "").replace(/[^\w'-]+/g, "|");
    
      was changed to
    
            input = input.replace(/[^\w0-9\\u ]+/, "").replace(/[ ]+/g, "|");

---
+++ hilitor-utf8.js
@@ -0,0 +1,204 @@
+// Original JavaScript code by Chirp Internet: www.chirp.com.au
+// Modified by Yanosh Kunsh to include utf-8 string comparison
+// Please acknowledge use of this code by including this header.
+
+function Hilitor(id, tag)
+{
+
+    var targetNode = document.getElementById(id) || document.body;
+    var hiliteTag = tag || "EM";
+    var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM)$");
+    var colors = ["#ff6", "#a0ffff", "#9f9", "#f99", "#f6f"];
+    var wordColor = [];
+    var colorIdx = 0;
+    var matchRegex = "";
+    var openLeft = false;
+    var openRight = false;
+
+    this.setMatchType = function(type)
+    {
+        switch(type)
+        {
+            case "left":
+                this.openLeft = false;
+                this.openRight = true;
+                break;
+            case "right":
+                this.openLeft = true;
+                this.openRight = false;
+                break;
+            case "open":
+                this.openLeft = this.openRight = true;
+                break;
+            default:
+                this.openLeft = this.openRight = false;
+        }
+    };
+
+    this.setRegex = function(input)
+    {
+        input = input.replace(/[^\w0-9\\u ]+/, "").replace(/[ ]+/g, "|");
+        var re = "(" + input + ")";
+        if(!this.openLeft) re = "\\b" + re;
+        if(!this.openRight) re = re + "\\b";
+        matchRegex = new RegExp(re, "i");
+    };
+
+    this.getRegex = function()
+    {
+        var retval = matchRegex.toString();
+        retval = retval.replace(/(^\/(\\b)?|\(|\)|(\\b)?\/i$)/g, "");
+        retval = retval.replace(/\|/g, " ");
+        return retval;
+    };
+
+    // recursively apply word highlighting
+    this.hiliteWords = function(node)
+    {
+        if(node == undefined || !node)
+            return;
+        if(!matchRegex)
+            return;
+        if(skipTags.test(node.nodeName))
+            return;
+
+        if(node.hasChildNodes()) {
+            for(var i=0; i < node.childNodes.length; i++)
+                this.hiliteWords(node.childNodes[i]);
+        }
+        if(node.nodeType == 3) { // NODE_TEXT
+            if((nv = node.nodeValue) && (regs = matchRegex.exec(nv))) {
+                if(!wordColor[regs[0].toLowerCase()]) {
+                    wordColor[regs[0].toLowerCase()] = colors[colorIdx++ % colors.length];
+                }
+
+                var match = document.createElement(hiliteTag);
+                match.appendChild(document.createTextNode(regs[0]));
+                match.style.backgroundColor = wordColor[regs[0].toLowerCase()];
+                match.style.fontStyle = "inherit";
+                match.style.color = "#000";
+
+                var after = node.splitText(regs.index);
+                after.nodeValue = after.nodeValue.substring(regs[0].length);
+                node.parentNode.insertBefore(match, after);
+            }
+        }
+        ;
+    };
+
+    // remove highlighting
+    this.remove = function()
+    {
+        var arr = document.getElementsByTagName(hiliteTag);
+        while(arr.length && (el = arr[0])) {
+            var parent = el.parentNode;
+            parent.replaceChild(el.firstChild, el);
+            parent.normalize();
+        }
+    };
+
+    // start highlighting at target node
+    this.apply = function(input)
+    {
+        input = convertCharStr2jEsc(input);
+        if(input === undefined || !input) {
+            this.remove();
+        }
+        this.remove();
+        this.setRegex(input);
+        this.hiliteWords(targetNode);
+    };
+
+}
+
+function dec2hex4(textString)
+{
+    var hexequiv = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F");
+    return hexequiv[(textString >> 12) & 0xF] + hexequiv[(textString >> 8) & 0xF]
+            + hexequiv[(textString >> 4) & 0xF] + hexequiv[textString & 0xF];
+}
+
+function convertCharStr2jEsc(str, cstyle)
+{
+    // Converts a string of characters to JavaScript escapes
+    // str: sequence of Unicode characters
+    var highsurrogate = 0;
+    var suppCP;
+    var pad;
+    var n = 0;
+    var outputString = '';
+    for(var i=0; i < str.length; i++) {
+        var cc = str.charCodeAt(i);
+        if(cc < 0 || cc > 0xFFFF) {
+            outputString += '!Error in convertCharStr2UTF16: unexpected charCodeAt result, cc=' + cc + '!';
+        }
+        if(highsurrogate != 0) { // this is a supp char, and cc contains the low surrogate
+            if(0xDC00 <= cc && cc <= 0xDFFF) {
+                suppCP = 0x10000 + ((highsurrogate - 0xD800) << 10) + (cc - 0xDC00);
+                if(cstyle) {
+                    pad = suppCP.toString(16);
+                    while(pad.length < 8) {
+                        pad = '0' + pad;
+                    }
+                    outputString += '\\U' + pad;
+                } else {
+                    suppCP -= 0x10000;
+                    outputString += '\\u' + dec2hex4(0xD800 | (suppCP >> 10)) + '\\u' + dec2hex4(0xDC00 | (suppCP & 0x3FF));
+                }
+                highsurrogate = 0;
+                continue;
+            } else {
+                outputString += 'Error in convertCharStr2UTF16: low surrogate expected, cc=' + cc + '!';
+                highsurrogate = 0;
+            }
+        }
+        if(0xD800 <= cc && cc <= 0xDBFF) { // start of supplementary character
+            highsurrogate = cc;
+        } else { // this is a BMP character
+            switch(cc)
+            {
+                case 0:
+                    outputString += '\\0';
+                    break;
+                case 8:
+                    outputString += '\\b';
+                    break;
+                case 9:
+                    outputString += '\\t';
+                    break;
+                case 10:
+                    outputString += '\\n';
+                    break;
+                case 13:
+                    outputString += '\\r';
+                    break;
+                case 11:
+                    outputString += '\\v';
+                    break;
+                case 12:
+                    outputString += '\\f';
+                    break;
+                case 34:
+                    outputString += '\\\"';
+                    break;
+                case 39:
+                    outputString += '\\\'';
+                    break;
+                case 92:
+                    outputString += '\\\\';
+                    break;
+                default:
+                    if(cc > 0x1f && cc < 0x7F) {
+                        outputString += String.fromCharCode(cc);
+                    } else {
+                        pad = cc.toString(16).toUpperCase();
+                        while(pad.length < 4) {
+                            pad = '0' + pad;
+                        }
+                        outputString += '\\u' + pad;
+                    }
+            }
+        }
+    }
+    return outputString;
+}
